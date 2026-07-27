432710d9c8b47bc2ae1f269fb0decf7b5c4a6b4e
Variiuz <alexander.pahl@frostfight.com>
2026-07-27 18:00:01 +0200

    fix(ci): install JDK 25 for paper-26 builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
